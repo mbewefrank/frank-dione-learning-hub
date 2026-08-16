e1645ca302a24294bd2c6ea09b6a534fe66fe762
mbewefrank <mbewefrank752@gmail.com>
2026-08-16 09:53:37 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_E7M_IvJn6U4-Q9tlaX7iug_Ft0W3fp_";
 const SUPABASE_URL = "https://pmwsecsmnyieeudqlbfd.supabase.co/rest/v1/";
 
+const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_PUBLISHABLE_KEY);
 function openModal(id) {
   const modal = document.getElementById(id);
 
